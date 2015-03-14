0d9e35d66c64933ace84c873a98ea5701ecff9a0
Nick Dingwall <ndingwall@gmail.com>
2015-03-14 11:46:21 +0000

    Finished code to compute cosine similarity for each trip.

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,7 +1,9 @@
 import numpy as np
 import csv
 from datetime import datetime
+from random import sample
 from scipy.spatial.distance import cosine
+import pickle
 
 def load_features():
     global num_features, features, drivers
@@ -34,23 +36,53 @@ feature_selection = [(x + 2) for x in feature_selection]
 
 load_features()
 
+def normalise_features(feature_list):
+    m = np.mean(feature_list)
+    v = np.var(feature_list)
+    features_out = (feature_list - m) / v
+    return features_out
+
 # for trip in features[driver]:
 start = datetime.now()
-driver = '1'
-cosine_differences = np.zeros([200,200])
-for j in range(200):
-    for i in range(200):
-        cosine_differences[j,i] = cosine(features[driver][j],features[driver][i])
+# drivers = sample(drivers, 10)
+cosines = {}
+drivrs = sorted(drivers)
+for driver in drivrs:
+    temp_features = np.asarray(features[driver])
+    for feature in range(1, num_features):
+        temp_features[:,feature] = normalise_features(temp_features[:,feature])
+    cosine_differences = np.zeros([200,200])
+    for j in range(200):
+        for i in range(200):
+            # cosine_differences[j,i] = cosine(features[driver][j],features[driver][i])
+            cosine_differences[j,i] = cosine(temp_features[j,1:],temp_features[i,1:])
+        cosine_differences[j,:].sort()
+        mean_diff = np.mean(cosine_differences[j,1:11])
+        if driver not in cosines:
+            cosines[driver] = [j, mean_diff]
+        else:
+            cosines[driver].append([j, mean_diff])
+    print 'Done driver %s, elapsed time: %s' % (driver, str(datetime.now() - start))
 
-print 'Done, elapsed time: %s' % str(datetime.now() - start)
 
-start = datetime.now()
-driver = '1'
-cosine_similarity = np.dot(features[driver], np.transpose(features[driver]))
+# start = datetime.now()
+# driver = '1'
+# cosine_similarity = np.dot(features[driver], np.transpose(features[driver]))
+#
+# cosine_differences = np.zeros([200,200])
+# for j in range(200):
+#     for i in range(200):
+#         cosine_differences[j,i] = cosine(features[driver][j],features[driver][i])
+# print 'Done, elapsed time: %s' % str(datetime.now() - start)
+# norms = np.linalg.norm(features[driver], 2, axis = 1)
+# norms_repeated = np.tile(norms, (200,1))
+# normaliser = norms_repeated * np.transpose(norms_repeated)
+# cosine_differences / normaliser
 
-cosine_differences = np.zeros([200,200])
-for j in range(200):
-    for i in range(200):
-        cosine_differences[j,i] = cosine(features[driver][j],features[driver][i])
+output = open('cosines.pkl', 'wb')
+pickle.dump(cosines, output)
+output.close()
 
-print 'Done, elapsed time: %s' % str(datetime.now() - start)
+# pkl_file = open('cosines.pkl', 'rb')
+# mydict2 = pickle.load(pkl_file)
+# pkl_file.close()
