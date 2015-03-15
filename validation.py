44281cc72758b9c2e6c3ef01fc3bb38817757cf4
kungfujam <james.f.owers+github@gmail.com>
2015-03-15 23:58:58 +0000

    Added plotting functionality

--- validation.py
+++ validation.py
@@ -5,6 +5,7 @@ import csv, os, json
 import sys
 from random import sample, shuffle
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.cross_validation import StratifiedKFold
@@ -33,7 +34,7 @@ def classify(trainData, trainLabels, testData, n_trees, depth, LRate):
     global importance
     y = np.ones((testData.shape[0],))
     #clf = GradientBoostingRegressor(n_estimators = n_trees, max_depth = depth, learning_rate = LRate)
-    clf = RandomForestClassifier(n_estimators=n_trees, oob_score=True ,max_depth = depth, n_jobs=-1)
+    clf = RandomForestClassifier(n_estimators=n_trees ,max_depth = depth, n_jobs=-1)
     clf.fit(trainData, trainLabels)
     # y = clf.predict(testData)
     y = clf.predict_proba(testData)[:,1]
@@ -115,23 +116,35 @@ def find_auc(driv, n_trees, depth, LRate):
     auc = auc[1:,:]
     meanAUC = np.mean(auc)
     varAUC = np.var(auc)
-    return meanAUC, varAUC
+    return auc, meanAUC, varAUC
 
 def grid_search(driv, list_trees, list_depth, list_lr, num_drivers):
     driv = sample(driv, num_drivers)
     shuffle(driv)
     auc_results = np.zeros([len(list_lr), len(list_depth), len(list_trees)])
     lrPos = 0
+    i = 0
     for lr in list_lr:
         depthPos = 0
         for depth in list_depth:
             estPos = 0
             for n_trees in list_trees:
-                thisMeanAUC, thisVarAUC = find_auc(driv, n_trees, depth, lr)
+                thisaucgrid, thisMeanAUC, thisVarAUC = find_auc(driv, n_trees, depth, lr)
                 auc_results[lrPos, depthPos, estPos] = thisMeanAUC
                 estPos += 1
                 print "\nRESULT: Mean AUC = %1.5f, Var AUC = %1.5f for %d estimators of max depth %d with learning rate %1.3f\n" % \
                     (thisMeanAUC, thisVarAUC, n_trees, depth, lr)
+                runningmean = np.divide(np.cumsum(np.mean(thisaucgrid,1)), range(1,(num_drivers+1)),dtype='float')
+                ax = plt.gca()
+                color_cycle = ax._get_lines.color_cycle
+                next_color = next(color_cycle)
+                plt.plot(range(num_drivers), np.mean(thisaucgrid,1), '-o', label="Config %d"%i, color=next_color)
+                plt.plot(range(num_drivers), runningmean, '--o',label="%d running mean"%i, color=next_color)
+                # plt.title("\nMean AUC = %1.5f, Var AUC = %1.5f for %d estimators of max depth %d\n" % \
+                #     (thisMeanAUC, thisVarAUC, n_trees, depth))
+                # handles, labels = ax.get_legend_handles_labels()
+                # ax.legend(handles, labels)
+                i += 1
             depthPos += 1
         lrPos += 1
     return auc_results
@@ -148,11 +161,15 @@ if __name__ == '__main__':
     try_n_trees = [500]
     try_depth = [4,5,6,7,8,9,10]
     try_lr = [0.05]
-    auc_results = grid_search(driv, try_n_trees, try_depth, try_lr, 5)
+    auc_results = grid_search(driv, try_n_trees, try_depth, try_lr, 500)
     auc = np.zeros([1,5]) # initialise table for results
     shuffle(driv)
     # for driver in driv:
     #    auc = np.vstack((auc,gen_validation(driver, 200, 5, 0.1)))
     #    print "Mean AUC so far: %1.4f. Just finished driver %d." % (np.mean(auc[1:,:]), int(driver))
     auc = auc[1:,:] # drop first row of auc (initialised to zero)
+    ax = plt.gca()
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+    plt.savefig("validation_auc.png")
     print 'Done, elapsed time: %s' % str(datetime.now() - start)
